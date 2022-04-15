d38586b7057d1044eac56d8295bf767eda2d3ae1
Wesley Bernardes <bw3sley@gmail.com>
2022-04-15 16:59:15 -0300

    :sparkles: feat: get users image

---
+++ main.js
@@ -0,0 +1,35 @@
+const url = "https://api.github.com/users/bw3sley";
+const token = "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"
+
+function GetUserImage() {
+    axios.get(url, {
+        headers: {
+            'Authorization': `token ${token}` 
+        }
+      })
+    .then(response => {
+        const data = response.data.avatar_url;
+
+        const userImageOne = document.querySelector("img#SideImages").setAttribute("src", data);
+        const userImageTwo = document.querySelector("img#PostImages").setAttribute("src", data);
+    })
+    .catch(error => alert(`Error: ${error}`))
+}
+
+GetUserImage();
+
+function GetUserLocation() {
+    axios.get(url, {
+        headers: {
+            'Authorization': `token ${token}` 
+        }
+    })
+    .then(response => {
+        const data = response.data.location;
+
+        const userLocation = document.querySelector("p#user-location").textContent = data
+    })
+    .catch(error => alert(`Error: ${error}`))
+}
+
+GetUserLocation();
