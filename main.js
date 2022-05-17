7aa71edd83a2c647801fa2c55eb141cc0cd94551
Wesley Bernardes <bw3sley@gmail.com>
2022-05-16 21:27:21 -0300

    :recycle: refactor: improve page accessibility

--- main.js
+++ main.js
@@ -14,14 +14,15 @@ function GetGitHubAPI(url, token) {
         const name = response.data.name;
         const login = response.data.login;
 
-        document.querySelector("img#SideImages").setAttribute("src", avatar_url);
-        document.querySelector("img#PostImages").setAttribute("src", avatar_url);
+        document.querySelector("img#side-image").setAttribute("src", avatar_url);
+        document.querySelector("img#post-image").setAttribute("src", avatar_url);
+
         document.querySelector("p#user-location").textContent = location;
         document.querySelector("a#user-company").textContent = company;
         document.querySelector("h2#user-name").textContent = name;
         document.querySelector("a#user-github").textContent = login;
     })
-    .catch(error => alert(`Error: ${error}`))
+    .catch(error => console.warn(`Error: ${error}`))
 }
 
 GetGitHubAPI(url, token);
